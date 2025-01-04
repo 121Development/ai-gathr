b0fb02be5b8ce16d6eb66274d933c7462aa0527d
Your Name <you@example.com>
2025-01-04 15:52:24 +0100

    feat: Add environment variable loading for Supabase client configuration

--- database.ts
+++ database.ts
@@ -5,3 +5,4 @@ const supabaseUrl = 'https://ymaplkjuewqkiajrdqll.supabase.co'
 const env = await load();
 const supabaseKey = env["SUPABASE_API_KEY"];
 const supabase = createClient(supabaseUrl, supabaseKey)
+
